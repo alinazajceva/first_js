4d85a3ae472fa80aef84787f3609338d40e74fca
Alina <alinka_zajceva@list.ru>
2021-07-11 21:32:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// const answer = +prompt("Вам есть 18?", " ");
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt('Имя?', '');
+// answers[1] = prompt('Фамилия?', '');
+// answers[2] = prompt('Сколько лет?', '');
+
+// console.log(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Alina';
+alert(`Hello, ${user}!`);
